b92fc09a59a9b443e0bf6b9f5b09e10399d4c6e7
shaheemMPM <shaheem.mpm@gmail.com>
2025-01-16 21:25:04 +0530

    Adds gif script

---
+++ gif.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import sys
+
+from PIL import Image
+from RGBMatrixEmulator import RGBMatrix, RGBMatrixOptions
+
+if len(sys.argv) < 2:
+    sys.exit("Require a gif argument")
+else:
+    image_file = sys.argv[1]
+
+gif = Image.open(image_file)
+
+try:
+    num_frames = gif.n_frames
+except Exception:
+    sys.exit("provided image is not a gif")
+
+
+# Configuration for the matrix
+options = RGBMatrixOptions()
+options.rows = 64
+options.cols = 64
+options.chain_length = 1
+options.parallel = 1
+options.hardware_mapping = "regular"  # If you have an Adafruit HAT: 'adafruit-hat'
+
+matrix = RGBMatrix(options=options)
+
+# Preprocess the gifs frames into canvases to improve playback performance
+canvases = []
+print("Preprocessing gif, this may take a moment depending on the size of the gif...")
+for frame_index in range(0, num_frames):
+    gif.seek(frame_index)
+    # must copy the frame out of the gif, since thumbnail() modifies the image in-place
+    frame = gif.copy()
+    frame.thumbnail((matrix.width, matrix.height), Image.LANCZOS)
+    canvas = matrix.CreateFrameCanvas()
+    canvas.SetImage(frame.convert("RGB"))
+    canvases.append(canvas)
+# Close the gif file to save memory now that we have copied out all of the frames
+gif.close()
+
+print("Completed Preprocessing, displaying gif")
+
+try:
+    print("Press CTRL-C to stop.")
+
+    # Infinitely loop through the gif
+    cur_frame = 0
+    while True:
+        matrix.SwapOnVSync(canvases[cur_frame])
+        if cur_frame == num_frames - 1:
+            cur_frame = 0
+        else:
+            cur_frame += 1
+except KeyboardInterrupt:
+    sys.exit(0)
